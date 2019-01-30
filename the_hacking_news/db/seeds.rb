c7c198d0a9a1006aaec57c2f347adf9fb2e7a4da
MargVander <margauxvanderbeken@gmail.com>
2019-01-30 22:41:38 +0100

    ajout readme et modif seed

--- the_hacking_news/db/seeds.rb
+++ the_hacking_news/db/seeds.rb
@@ -1,16 +1,16 @@
 require 'faker'
 
-# 20.times do
-#   user = User.create!(email: Faker::Internet.email, username: Faker::Internet.username)
-# end
+20.times do
+  user = User.create!(email: Faker::Internet.email, username: Faker::Internet.username)
+end
 
-# 30.times do
-#   link = Link.create!(user_id: rand(1..20), url: Faker::Internet.url)
-# end
+30.times do
+  link = Link.create!(user_id: rand(1..20), url: Faker::Internet.url)
+end
 
-# 40.times do
-#   com = Comment.create!(user_id: rand(1..20), link_id: rand(1..30), content: Faker::Simpsons.quote)
-# end
+40.times do
+  com = Comment.create!(user_id: rand(1..20), link_id: rand(1..30), content: Faker::Simpsons.quote)
+end
 
 50.times do
   ccom = ComComment.create!(user_id: rand(1..20), comment_id: rand(1..40), content: Faker::Simpsons.quote)
